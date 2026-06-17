2dc17d80cdb51cd46f818723963f8e14938ba4ef
Hubert <hub300pl@gmail.com>
2026-06-17 23:12:24 +0200

    .

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,7 +1,31 @@
 import pandas as pd
+import re
+
 def upload_tsv(path, col_drop=None, sep='\t', header=None):
     df = pd.read_csv(path, sep=sep, header=header)
     if col_drop:
         df = df.drop(col_drop, axis=1)
     df.columns = ['eng_text', 'pol_text']
     return df
+
+# def tokenize_snt(snt, is_eng=True):
+#     if is_eng:
+#         cases_del = r"""['"€;:()$%–—‘’°“”₳+&#=‐…−/£@-]"""
+#         tok_func = lambda x: x + ['<eos>']
+#     else:    
+#         cases_del =  r"""[]„”€…";:()'%/’$\u200b\xad–°+₳#'\[=−­`—“@«»-]"""
+#         tok_func = lambda x: ['<bos>'] + x + ['<eos>']
+        
+#     snt = re.sub(cases_del, '', snt.lower())
+#     snt = re.sub(r'\s*([.,!?])\s*', r' \1 ', snt)
+#     return tok_func(re.split(r'\s+', snt.strip()))
+
+def tokenize_eng(snt):
+    snt = re.sub(r"""['"€;:()$%–—‘’°“”₳+&#=‐…−/£@-]""", '', snt.lower())
+    snt = re.sub(r'\s*([.,!?])\s*', r' \1 ', snt)
+    return re.split(r'\s+', snt.strip()) + ['<eos>']
+
+def tokenize_pol(snt):
+    snt = re.sub(r"""[]„”€…";:()'%/’$\u200b\xad–°+₳#'\[=−­`—“@«»-]""", '', snt.lower())
+    snt = re.sub(r'\s*([.,!?])\s*', r' \1 ', snt)
+    return ['<bos>'] + re.split(r'\s+', snt.strip()) + ['<eos>']
--- todo.md
+++ todo.md
@@ -1,1 +1,4 @@
-### asds
+## TO DO - Data.py:
+### Tokenizacja
+- [ ] Popraw tokenizacje zdan uwzglednij inne znaki specjalne oraz ogarnij przypadki np: that's itd.
+- [ ] Uwzglednij roznice miedzy '.', a '...'
