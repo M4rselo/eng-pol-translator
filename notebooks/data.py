9d635e9f5a95cd1f2445e5ea9b84cb4009bcdf33
Hubert <hub300pl@gmail.com>
2026-06-19 01:34:14 +0200

    data.py 1.0 & 01.Scratchpad Init for Testing

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,3 +1,6 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 import pandas as pd
 import re
 from collections import Counter
@@ -57,13 +60,33 @@ def tokens_to_id(df, src_col, tgt_col, min_freq=2):
 
     eng_unk, pol_unk = eng_vcb['<unk>'], pol_vcb['<unk>']
     df_proc['src_ids'] = df_proc[src_col].apply(lambda snt: [eng_vcb.get(tok, eng_unk) for tok in snt])
-    df_proc['lbl_ids'] = df_proc[tgt_col].apply(lambda snt: [pol_vcb.get(tok, pol_unk) for tok in snt])
+    df_proc['tgt_ids'] = df_proc[tgt_col].apply(lambda snt: [pol_vcb.get(tok, pol_unk) for tok in snt])
     return df_proc, eng_vcb, pol_vcb
 
 def shuffle_split(df, tr_per):
     df = df.sample(frac=1).reset_index(drop=True)
     split_index = int(len(df) * tr_per)
     train_df = df.iloc[:split_index] 
-    val_df = df.iloc[split_index:]
+    val_df = df.iloc[split_index:].reset_index(drop=True)
     return train_df, val_df
-    
+
+class EngPolDataset(Dataset):
+    def __init__(self, df, src_col, lbl_col):
+        self.src_eng = df[src_col]
+        self.tgt_pol = df[lbl_col]
+
+    def __len__(self):
+        return len(self.src_eng)
+
+    def __getitem__(self, idx):
+        return torch.tensor(self.src_eng[idx]), torch.tensor(self.tgt_pol[idx])
+
+def collate_fn(batch):
+    eng_batch, pol_batch = zip(*batch)
+    eng_padded = pad_sequence(eng_batch, batch_first=True, padding_value=0)
+    pol_padded = pad_sequence(pol_batch, batch_first=True, padding_value=0)
+    eng_val_lens = (eng_padded != 0).sum(dim=-1, keepdims=True)
+    return eng_padded, pol_padded, eng_val_lens
+
+def data_loader(data, batch_size):
+    return DataLoader(data, batch_size=batch_size, collate_fn=collate_fn)
